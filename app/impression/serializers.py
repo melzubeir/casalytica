32e0f65b50b00be37483237e25a24fc377ff2ef9
Mo Elzubeir <elzubeir@gmail.com>
2022-11-11 14:47:47 -0600

    resolve metadata from input payload fields

--- app/impression/serializers.py
+++ app/impression/serializers.py
@@ -2,6 +2,7 @@
 serializers for impression api
 """
 from rest_framework import serializers
+from user_agents import parse
 from impression.models import (
     Impression
 )
@@ -9,13 +10,25 @@ from django.contrib.gis.geoip2 import GeoIP2
 
 
 class ImpressionSerializer(serializers.ModelSerializer):
-    """serializer for impression model"""
+    """serializer for impression model
+
+    sample payload:
+    {
+        "post_hash": "3151efdf499e5220241b48040fc9500970f4aaa6f512daa0b88be7b3f200c339",
+        "source_app": 1,
+        "remote_addr": "4.2.2.1",
+        "referer": "https://www.google.com/",
+        "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
+    }
+    """
     class Meta:
         model = Impression
         fields = [
             'id', 'created', 'post_hash', 'is_deso',
+            'remote_addr', 'user_agent', 'referer',
+            'source_app',
         ]
-        read_only_fields = ['-created']
+        read_only_fields = ['created']
 
     def create(self, validated_data):
         """create a new impression"""
@@ -23,10 +36,13 @@ class ImpressionSerializer(serializers.ModelSerializer):
         auth_user = self.context['request'].user
         impression = Impression.objects.create(
             **validated_data, user=auth_user)
-        remote_addr = validated_data.pop('remote_addr', None)
-        self.user_agent = validated_data.pop('user_agent', None)
 
-        location = self.get_location(remote_addr)
+        self.remote_addr = validated_data.pop('remote_addr', None)
+        self.user_agent = validated_data.pop('user_agent', None)
+        self.referer = validated_data.pop('referer', None)
+        self.source_app = validated_data.pop('source_app', None)
+
+        location = self.get_location(self.remote_addr)
         if location:
             impression.city = location['city'] if True else 'unknown'
             impression.country = location['country_name'] if True else 'unknown'
@@ -34,11 +50,15 @@ class ImpressionSerializer(serializers.ModelSerializer):
             impression.longitude = location['longitude'] if True else 0
             impression.tz = location['time_zone'] if True else 'unknown'
 
-        self.referer = validated_data.pop('referer', None)
-        self.device_type = validated_data.pop('device_type', None)
-        self.os_type = validated_data.pop('os_type', None)
-        self.os_version = validated_data.pop('os_version', None)
-        self.broser_type = validated_data.pop('browser_type', None)
+        if self.user_agent:
+            ua = parse(self.user_agent)
+            impression.device_brand = ua.device.brand
+            impression.device_family = ua.device.family
+            impression.device_model = ua.device.model
+            impression.os_family = ua.os.family
+            impression.os_version = ua.os.version_string
+            impression.browser_family = ua.browser.family
+            impression.browser_version = ua.browser.version_string
 
         # maybe spend some time validating data? lol
         return impression
@@ -68,8 +88,9 @@ class ImpressionDetailSerializer(ImpressionSerializer):
 
     class Meta(ImpressionSerializer.Meta):
         fields = ImpressionSerializer.Meta.fields + [
-            'source_app', 'referer', 'remote_addr', 'user_agent',
-            'device_type', 'browser_type', 'browser_version', 'os_type',
-            'os_version', 'city', 'country', 'latitude', 'longitude',
+            'device_brand', 'device_family', 'device_model',
+            'browser_family', 'browser_version',
+            'os_family', 'os_version',
+            'city', 'country', 'latitude', 'longitude',
             'tz',
         ]
