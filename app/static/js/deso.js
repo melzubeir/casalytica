7ea142236a99f1fe5790c909d161e6b95ad05eaf
Mo Elzubeir <elzubeir@gmail.com>
2022-11-29 14:19:46 -0600

    dunno.. out of ideas.

--- app/static/js/deso.js
+++ app/static/js/deso.js
@@ -1,56 +1,97 @@
+// Create a new window for the login flow.
 function login() {
-    identityWindow = window.open('https://identity.deso.org/log-in?accessLevelRequest=2', null, 'toolbar=no, width=800, height=1000, top=0, left=0');
+  identityWindow = window.open('https://identity.deso.org/log-in?accessLevelRequest=' +
+    accessLevel, null, 'toolbar=no, width=800, height=1000, top=0, left=0');
+
 }
 
+// Check if the window is closed.
 function handleInit(e) {
-    if (!init) {
-        init = true;
-        iframe = document.getElementById("identity");
+  if (!init) {
+    init = true;
 
-        for (const e of pendingRequests) {
-            postMessage(e);
-        }
-
-        pendingRequests = []
+    for (const e of pendingRequests) {
+      e.source.postMessage(e, "*");
     }
-    respond(e.source, e.data.id, {})
+
+    pendingRequests = []
+    pm_id = e.data.id
+    source = e.source
+  }
+  respond(e.source, e.data.id, {})
 }
 
+// Handle a message from the login window.
 function handleLogin(payload) {
-    console.log(payload);
-    if (identityWindow) {
-        identityWindow.close();
-        identityWindow = null;
+  user = payload['users'][payload.publicKeyAdded]
+  user['publicKey'] = payload.publicKeyAdded;
 
-        var element = document.getElementById('loggedin');
-        element.innerText = 'Logged in as ' + payload.publicKeyAdded;
+  if (identityWindow) {
+    if (JWT === false) {
+      identityWindow.close();
+      identityWindow = null;
+      resolve(user)
+    } else {
+      let payload = {
+        accessLevel: user.accessLevel,
+        accessLevelHmac: user.accessLevelHmac,
+        encryptedSeedHex: user.encryptedSeedHex
+      };
+      source.postMessage({
+        id: pm_id,
+        service: 'identity',
+        method: 'jwt',
+        payload: payload
+      }, "*");
     }
+  }
 }
+
+
+function handleJWT(payload) {
+  user['jwt'] = payload['jwt'];
+  if (identityWindow) {
+    identityWindow.close();
+    identityWindow = null;
+  }
+  resolve(user);
+}
+
 
 function respond(e, t, n) {
-    e.postMessage({
-        id: t,
-        service: "identity",
-        payload: n
-    }, "*")
+  e.postMessage({
+    id: t,
+    service: "identity"
+  }, "*")
 }
 
-function postMessage(e) {
-    init ? this.iframe.contentWindow.postMessage(e, "*") : pendingRequests.push(e)
-}
 
-// const childWindow = document.getElementById('identity').contentWindow;
 window.addEventListener('message', message => {
-    const {data: {id: id, method: method, payload: payload}} = message;
+  console.log('Listening: ', message)
+  const {data: {id: id, service: service, method: method, payload: payload}} = message;
 
-    if (method == 'initialize') {
-        handleInit(message);
-    } else if (method == 'login') {
-        handleLogin(payload);
-    }
+  if (service !== "identity") {
+    console.log("invalid service: ", service);
+
+    return
+  };
+  if (method === 'initialize') {
+    console.log('initialize');
+    handleInit(message);
+  } else if (method === 'login') {
+    console.log('login');
+    handleLogin(payload);
+  } else if ('jwt' in payload) {
+    console.log('jwt');
+    handleJWT(payload);
+  }
 });
 
-var init = false;
-var iframe = null;
-var pendingRequests = [];
-var identityWindow = null;
+let init = false;
+let accessLevel = 2;
+let JWT = false;
+let pm_id = ''
+let source = null;
+let user = null;
+let pendingRequests = [];
+let identityWindow = null;
