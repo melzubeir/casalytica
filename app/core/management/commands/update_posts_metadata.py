d2cbc62e08fe99528327f39b27cd6817733de24d
Mo Elzubeir <elzubeir@gmail.com>
2022-12-04 22:15:24 -0600

    script to update posts data with latest likes/reposts/etc from blockchain

--- app/core/management/commands/update_posts_metadata.py
+++ app/core/management/commands/update_posts_metadata.py
@@ -1,16 +1,25 @@
 """
 Update posts metadata from the deso blockchain
 """
-import time
+
+from datetime import (
+    datetime,
+    timedelta,
+)
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from django.db.models import F
+from django.db.models import F, Q
 from django.conf import settings
-from analytics.models import Post, Node
+from analytics.models import Post
+import deso
+
 
 
 class Command(BaseCommand):
     help = 'Update posts metadata from the deso blockchain'
+
+    desoPost = deso.Posts()
+    num_days = 1
 
     def handle(self, *args, **options):
         self.stdout.write('Updating posts metadata..')
@@ -19,29 +28,17 @@ class Command(BaseCommand):
 
     def update_posts_metadata(self):
         # get the most recent post
-        post = Post.objects.order_by('-id').first()
-        # get the most recent post from the blockchain
-        node = Node.objects.get(id=settings.DESO_NODE_ID)
-        url = f'{node.url}/api/v0/get-app-state'
-        data = {
-            'PublicKeyBase58Check': settings.DESO_PUBLIC_KEY,
-            'ReaderPublicKeyBase58Check': settings.DESO_PUBLIC_KEY,
-            'GetPostsState': True,
-            'PostHashHex': post.post_hash,
-        }
-        response = requests.post(url, data=data)
-        if response.status_code == 200:
-            posts = response.json()['PostsFound']
-            for post in posts:
-                # update the post
-                Post.objects.filter(post_hash=post['PostHashHex']).update(
-                    impressions_total=F('impressions_total') +
-                    post['Impressions'],
-                    likes_total=F('likes_total') + post['LikeCount'],
-                    diamonds_total=F('diamonds_total') + post['DiamondCount'],
-                    comments_total=F('comments_total') + post['CommentCount'],
-                    reposts_total=F('reposts_total') + post['RepostCount'],
-                )
-                time.sleep(1)
-        else:
-            raise Exception('Error getting posts from the deso blockchain')
+        posts = Post.objects.filter(
+            Q(last_sync__lte=datetime.now() - timedelta(days=self.num_days)) |  Q(last_sync__isnull=True)
+            )
+
+        for post in posts:
+            myPost = self.desoPost.getSinglePost(post.post_hash).json()['PostFound']
+            Post.objects.filter(post_hash=myPost['PostHashHex']).update(
+                likes_total=F('likes_total') + myPost['LikeCount'],
+                diamonds_total=F('diamonds_total') + myPost['DiamondCount'],
+                comments_total=F('comments_total') + myPost['CommentCount'],
+                reposts_total=F('reposts_total') + myPost['RepostCount'],
+                last_sync=datetime.now()
+            )
+            print("Updating post: " + myPost['PostHashHex'])
